96d96eb55fd638c9223ea3b2ad46db6d116157f3
bamalam <joshlovellsmith@hotmail.com>
2022-08-29 15:25:15 +1200

    add serverside validation

--- server/index.ts
+++ server/index.ts
@@ -36,24 +36,66 @@ app.get("/api", (req, res) => {
 
 // post a notification request to the server
 
+function constraintsValid(constraints: Partial<WeatherConstaint>): boolean {
+  // todo more checks
+  if (constraints.windSpeed) {
+    if (!constraints.windSpeed.min && !constraints.windSpeed.max) {
+      return false;
+    }
+  }
+  if (constraints.temperature) {
+    if (!constraints.temperature.min && !constraints.temperature.max) {
+      return false;
+    }
+  }
+  if (constraints.humidity) {
+    if (!constraints.humidity.min && !constraints.humidity.max) {
+      return false;
+    }
+  }
+  return true;
+}
+
 app.post("/api/notificationSub", async (req, res) => {
   try {
     await client.connect();
 
     const db = client.db();
     // todo
-    console.log(req.body)
-    const notificationRequest = req.body as WeatherNotificationSubscription;
+    console.log(req.body);
+    const { location, constraints, email } = req.body;
+
     // validate the request
+    if (!location || typeof location !== "string") {
+      res.status(400).send("Missing location");
+      return;
+    }
+
+    if (!email || typeof email !== "string" || !email.includes("@")) {
+      res.status(400).send("Missing email");
+      return;
+    }
+
+    if (
+      !constraints ||
+      typeof constraints !== "object" ||
+      !constraintsValid(constraints)
+    ) {
+      res.status(400).send("Missing constraints");
+      return;
+    }
+
+    const notificationRequest: WeatherNotificationSubscription = {
+      email: email,
+      location: location,
+      constraints: constraints,
+    };
+
     // store it in the database in the subs collection
 
     // after validation create a document to then insert
-    const subscriptionDoc: WeatherNotificationSubscription = {
-      email: notificationRequest.email,
-      location: notificationRequest.location,
-      constraints: notificationRequest.constraints,
-    };
-    await db.collection("subs").insertOne(subscriptionDoc);
+
+    await db.collection("subs").insertOne(notificationRequest);
 
     res.send("Subscribed!");
   } catch (error) {
