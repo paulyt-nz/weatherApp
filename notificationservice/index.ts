a5c1dc4580712d9e00257bcc17ac0223fff3464e
Paul Thornton <p.d.thornton995@gmail.com>
2022-09-07 16:42:01 +1200

    added weather check functions, notificationservice

--- notificationservice/index.ts
+++ notificationservice/index.ts
@@ -1,8 +1,8 @@
 import { setTimeout } from "timers/promises";
 import { WindDirection, WeatherNotificationSubscription, WeatherConstaint, Weather } from '../common/weather'
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 
-async function connectToDB() {
+async function connectToDB(): Promise<Db> {
   const uri = process.env.MONGO_DB;
   if (!uri) throw new Error("Missing MONGO_DB connection string from .env vars");
 
@@ -10,27 +10,38 @@ async function connectToDB() {
 
   try {
     await client.connect()
-    console.log('Connected to database')
+    return client.db()
   }
   catch (err) {
     console.log(err)
     throw err
   }
-  return client
 }
 
-async function getRequests(): Promise<WeatherNotificationSubscription[]> {
+async function getRequests(db: Db): Promise<WeatherNotificationSubscription[]> {
   // todo check database for requests
   // todo support pagination or streaming
+  // todo look into what a generic is
+  const collection = db.collection<WeatherNotificationSubscription>('subs');
 
-
-  // need to pull the request out of the database to an array and return the array
-
-  return [];
+  return collection.find().toArray()
 }
 
-function getWeather(location: string): Promise<Weather | null> {
-  throw new Error("Function not implemented.");
+async function getWeather(location: string): Promise<Weather | null> {
+   const getRandomWind = (): WindDirection => {
+      const directions: WindDirection[] = ["N" , "NE" , "E" , "SE",  "S" , "SW" , "W" , "NW"]
+      return directions[Math.floor(Math.random() * directions.length)]
+   }
+
+
+    const weather: Weather = {
+      humidity: Math.random() * 100,
+      temperature: Math.random() * 40,
+      windDir: getRandomWind(),
+      windSpeed: Math.random() * 100
+
+  }
+  return Promise.resolve(weather)
 }
 
 function createNotification(
@@ -39,7 +50,15 @@ function createNotification(
 ): string {
   // this is where you would create a nice email message
   // e.g. the weather is going to be x at y matching your constraints
-  throw new Error("Function not implemented.");
+  
+  const { location } = request;
+
+  const { windSpeed, windDir, humidity, temperature } = request.constraints
+
+
+  let notification = `Looks like the weather at ${location} is just what you were after!`
+
+  return notification
 }
 
 async function sendNotification(notification: string): Promise<boolean> {
@@ -47,17 +66,65 @@ async function sendNotification(notification: string): Promise<boolean> {
   throw new Error("Function not implemented.");
 }
 
-function checkWeatherMatchesConstraints(
+// todo use jest to test
+export function checkWeatherMatchesConstraints(
   constraints: WeatherConstaint,
   weather: Weather
 ): boolean {
   // todo check if weather matches
-  throw new Error("Function not implemented.");
+
+    if(constraints.windSpeed) {
+      if (constraints.windSpeed.min !== undefined) {
+        if(weather.windSpeed < constraints.windSpeed.min) {
+          return false;
+        }
+      }
+      if (constraints.windSpeed.max !== undefined) {
+        if(weather.windSpeed < constraints.windSpeed.max) {
+          return false;
+        }
+      }
+    }
+
+    if(constraints.temperature) {
+      if (constraints.temperature.min !== undefined) {
+        if(weather.temperature < constraints.temperature.min) {
+          return false;
+        }
+      }
+      if (constraints.temperature.max !== undefined) {
+        if(weather.temperature < constraints.temperature.max) {
+          return false;
+        }
+      }
+    }
+
+    if(constraints.humidity) {
+      if (constraints.humidity.min !== undefined) {
+        if(weather.humidity < constraints.humidity.min) {
+          return false;
+        }
+      }
+      if (constraints.humidity.max !== undefined) {
+        if(weather.humidity < constraints.humidity.max) {
+          return false;
+        }
+      }
+    }
+
+    if(constraints.windDir !== undefined) {
+      let windDirMatch = constraints.windDir.some(dir => dir === weather.windDir) // true or false
+      
+      if(windDirMatch === false) {
+        return false
+      }
+    }
+  return true
 }
 
-async function poll() {
+async function poll(db: Db) {
   while (true) {
-    const requests = await getRequests();
+    const requests = await getRequests(db);
     for (const request of requests) {
       const weather = await getWeather(request.location);
       if (!weather) {
@@ -75,4 +142,4 @@ async function poll() {
   }
 }
 
-connectToDB().then(() => poll());
+connectToDB().then((db) => poll(db));
