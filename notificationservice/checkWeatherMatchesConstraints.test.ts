fd9a3528c5fff3e4421d15ba162e4ee215d94d33
Paul Thornton <p.d.thornton995@gmail.com>
2022-09-13 10:14:07 +1200

    first test with jest - not quite working

--- notificationservice/checkWeatherMatchesConstraints.test.ts
+++ notificationservice/checkWeatherMatchesConstraints.test.ts
@@ -1,4 +1,33 @@
 import {checkWeatherMatchesConstraints} from './index'
+import {expect, jest, test} from '@jest/globals';
+import { WeatherConstaint, Weather } from '../common/weather'
 
-// set up a jest test to see check that my checkWeatherMatchesConstraints() function is working properly
 
+// checks to run
+//  - Each constraint with one pass and one fail - will need a correct wind dir (we have required this)
+//  - wind dir pass and fails with nothing else
+
+
+test("test if wind speed in correct range gives true", () =>{
+    //Arrange - get the data together for testing
+    const weatherTestValues: Weather = {
+        windDir: 'N',
+        windSpeed: 20,
+        humidity: 50,
+        temperature: 25
+    }
+    const constraintTestValues: WeatherConstaint = {
+        windDir: ['N'],
+        windSpeed: {
+            min: 10,
+            max: 15
+        }
+
+    }
+
+    //Act - run the function and save the result
+    const result = checkWeatherMatchesConstraints(constraintTestValues, weatherTestValues)
+
+    //Assert - run the testing functions
+    expect(result).toBe(false)
+})
