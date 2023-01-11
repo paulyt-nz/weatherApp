841feff8ea01fc63ab6b43dc70708f40a25f9848
Paul Thornton <p.d.thornton995@gmail.com>
2023-01-11 15:24:02 +1300

    convertLocationToCoords moved to location.ts

--- server/index.ts
+++ server/index.ts
@@ -4,14 +4,9 @@ import express from "express";
 
 import { client } from "./db/db";
 import { WindDirection, WeatherNotificationSubscription, WeatherConstaint } from '../common/weather'
-import fetch from 'node-fetch';
+import { convertLocationToCoords } from "./location";
 
 const port = process.env.PORT ?? 3000;
-const MapBoxApiKey = process.env.MAPBOX;
-
-if (!MapBoxApiKey) {
-  throw new Error("Missing MAPBOX API key from .env vars");
-}
 
 const app = express();
 
@@ -42,28 +37,6 @@ function constraintsValid(constraints: Partial<WeatherConstaint>): boolean {
     }
   }
   return true;
-}
-
-async function convertLocationToCoords (location: string): Promise<number[] | null> {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${MapBoxApiKey}`
-
-  console.debug(url)
-
-  try {
-      const res = await fetch(url)
-      console.debug("res:  ", res)
-      const data = await res.json() as any
-      console.debug("data:  ",data)
-
-      const [lon, lat] : number[] = data.features[0].center
-      console.debug('lat: ', lat, 'long: ', lon)
-
-      return Promise.resolve([lat, lon])
-  }
-  catch (err) {
-      console.error('ERROR', err)
-      return null
-  }
 }
 
 
---
+++ server/location.ts
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+const MapBoxApiKey = process.env.MAPBOX;
+
+if (!MapBoxApiKey) {
+    throw new Error("Missing MAPBOX API key from .env vars");
+}
+
+export async function convertLocationToCoords (location: string): Promise<number[] | null> {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${MapBoxApiKey}`
+  
+    console.debug(url)
+  
+    try {
+        const res = await fetch(url)
+        console.debug("res:  ", res)
+        const data = await res.json() as any
+        console.debug("data:  ",data)
+  
+        const [lon, lat] : number[] = data.features[0].center
+        console.debug('lat: ', lat, 'long: ', lon)
+  
+        return Promise.resolve([lat, lon])
+    }
+    catch (err) {
+        console.error('ERROR', err)
+        return null
+    }
+  }
