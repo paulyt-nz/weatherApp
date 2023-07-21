de1240dbdc98b69d2d63e2b8a938f7b474f7cd70
pdthornton <111858013+pdthornton@users.noreply.github.com>
2023-07-22 08:57:18 +1200

    toggle functionality added to form

--- web-nextjs/src/app/app/page.tsx
+++ web-nextjs/src/app/app/page.tsx
@@ -1,14 +1,18 @@
+"use client";
+
+import { useState } from "react"
 import Footer from "../Footer"
 import Navbar from "../Navbar"
 
 export default function MainApp() {
 
+
+  const [ showWindDirection, setShowWindDirection ] = useState(true);
+  const [ showWindSpeed, setShowWindSpeed ] = useState(true);
+  const [ showTemp, setShowTemp ] = useState(true);
+  const [ showHumidity, setShowHumidity ] = useState(false);
+
   
-
-
-
-
-
   return (
     <main className="">
         <Navbar />
@@ -17,6 +21,33 @@ export default function MainApp() {
 
 
         <div className="">
+
+          <div className="">
+            <fieldset>
+              <legend>Choose your parameters</legend>
+
+              <div>
+                <input type="checkbox" id="windDirectionCheckbox" name="windDirectionCheckbox" checked={showWindDirection} onChange={() => setShowWindDirection(!showWindDirection)} />
+                <label htmlFor="windDirectionCheckbox">Wind Direction</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="windSpeedCheckBox" name="windSpeedCheckBox" checked={showWindSpeed} onChange={() => setShowWindSpeed(!showWindSpeed)} />
+                <label htmlFor="windSpeedCheckBox">Wind Speed</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="tempCheckbox" name="tempCheckbox" checked={showTemp} onChange={() => setShowTemp(!showTemp)} />
+                <label htmlFor="tempCheckbox">Temperature</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="humidityCheckbox" name="humidityCheckbox" checked={showHumidity} onChange={() => setShowHumidity(!showHumidity)}/>
+                <label htmlFor="humidityCheckbox">Humidity</label>
+              </div>
+
+            </fieldset>
+          </div>
 
           <form>
             <div className="">
@@ -29,6 +60,7 @@ export default function MainApp() {
               <input className="" type="text" id="location" name="location" />
             </div>
 
+            {showWindDirection && (
             <div className="">
               <label className="" htmlFor="windDirection">Wind Direction</label>
               <select className="" name="windDirection" id="windDirection" multiple>
@@ -41,8 +73,9 @@ export default function MainApp() {
                 <option value="W">W</option>
                 <option value="NW">NW</option>
               </select>
-            </div>
+            </div>)}
 
+            {showWindSpeed && (  
             <div className="">
               <p>Wind Speed</p>
               <label className="" htmlFor="windSpeedMin">from</label>
@@ -50,17 +83,19 @@ export default function MainApp() {
              
               <label className="" htmlFor="windSpeedMax">to</label>
               <input className="" type="number" id="windSpeedMax" name="windSpeedMax" min="0" max="100" placeholder="50kph" />
-            </div>
+            </div>)}
 
+            {showTemp && (
             <div className="">
               <p>Temperature</p>
               <label className="" htmlFor="tempMin">from</label>
-              <input className="" type="number" id="tempMin" name="tempMin" min="-10" max="45" placeholder="-10&#8451" />
+              <input className="" type="number" id="tempMin" name="tempMin" min="-10" max="45" placeholder="-10°C" />
              
               <label className="" htmlFor="tempMax">to</label>
-              <input className="" type="number" id="tempMax" name="tempMax" min="-10" max="45" placeholder="40&#8451" />
-            </div>
+              <input className="" type="number" id="tempMax" name="tempMax" min="-10" max="45" placeholder="45°C" />
+            </div>)}
 
+            {showHumidity && (
             <div className="mb-3">
               <p>Humidity</p>
               <label className="" htmlFor="humidityMin">from</label>
@@ -68,7 +103,7 @@ export default function MainApp() {
               
               <label className="" htmlFor="humidityMax">to</label>
               <input className="" type="number" id="humidityMax" name="humidityMax" min="10" max="100" placeholder="100%" />
-            </div>
+            </div>)}
 
             <div className="">
               <input className="btn btn-success" type="submit" value="Submit" id="submit" />
