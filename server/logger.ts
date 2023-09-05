c84d77d0a8ed0f62d60f36816c4cc60dab7ce576
pdthornton <111858013+pdthornton@users.noreply.github.com>
2023-09-06 08:23:15 +1200

    logger added using winston

---
+++ server/logger.ts
@@ -0,0 +1,16 @@
+import winston from "winston";
+
+const { combine, timestamp, colorize, align, printf } = winston.format;
+
+export const log = winston.createLogger({
+    level: 'debug',
+    format: combine(
+        colorize({ all: true }),
+        timestamp({
+          format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+        }),
+        align(),
+        printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+      ),
+    transports: [new winston.transports.Console()],
+  });
