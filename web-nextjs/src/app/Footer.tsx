1dcff9c126ed98de442fe4d00bf96f9b5fa4573a
pdthornton <111858013+pdthornton@users.noreply.github.com>
2023-09-06 06:40:10 +1200

    landing page photo and styles added

--- web-nextjs/src/app/Footer.tsx
+++ web-nextjs/src/app/Footer.tsx
@@ -1,8 +1,15 @@
-export default function Footer() {
+import { CSSProperties } from 'react';
+
+interface FooterProps {
+    style?: CSSProperties;
+  }
+
+export default function Footer({ style }: FooterProps ) {
     return (
-        <footer className="bg-gray-800 text-white py-6">
+        <footer className="bg-gray-800 text-white py-6" style={style}>
             <div className="container mx-auto flex justify-between items-center">
-                <span className="block mb-2">This is a Footer</span>
+                <span className="block mb-2">© 2023 Adventure Alarm</span>
+                <span className="block mb-2"></span>
                 <span className="block mb-2">Created by PaulyT</span>
             </div>
         </footer>
--- web-nextjs/src/app/page.tsx
+++ web-nextjs/src/app/page.tsx
@@ -1,19 +1,37 @@
+import { CSSProperties } from 'react';
 import Link from 'next/link';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import homepagePhoto from './homepage-photo.jpg';
 
 export default function Home() {
-  return (
-    <main className="">
-      <Navbar />
 
-      <div>
-        <h1>THIS WILL BE MY LANDING/HOME PAGE</h1>
-        <Link id="enter" href="/app">Enter App</Link>
+  const navbarStyles: CSSProperties = {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    backdropFilter: 'blur(1px)',
+    color: 'white'
+  }
+
+  const footerStyles: CSSProperties = {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    backdropFilter: 'blur(1px)',
+    color: 'white'
+  }
+    
+  return (
+    <main className="flex flex-col justify-between min-h-screen bg-cover bg-center" style={{backgroundImage: `url(${homepagePhoto.src})`}}>
+      <Navbar style={navbarStyles}/>
+
+      <div className="self-center flex flex-col items-center mt-20 mb-20 p-6" >
+        <h1 className="text-4xl text-white font-semibold mb-8" 
+          style={{ textShadow: '4px 4px 10px rgba(0, 0, 0, 1)' }}>
+            Welcome to your Adventure Alarm
+        </h1>
+        <Link className="bg-black bg-opacity-60 text-gray-300 font-bold p-4 rounded hover:bg-opacity-40 hover:text-white transition duration-150" id="enter" href="/app">Enter App</Link>
         <p></p>
       </div>
 
-      <Footer />
+      <Footer style={footerStyles}/>
     </main>
   )
 }
