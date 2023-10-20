595c9a7b85916bae647833665fc5e1391871dcfe
pdthornton <111858013+pdthornton@users.noreply.github.com>
2023-10-21 08:26:39 +1300

    aboutBackground.jpg issue fixed

--- web-nextjs/src/app/about/page.tsx
+++ web-nextjs/src/app/about/page.tsx
@@ -2,7 +2,7 @@ import { CSSProperties } from 'react';
 import Link from 'next/link';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
-import aboutBackground from './aboutbackground.jpg';
+import aboutBackground from './aboutBackground.jpg';
 import { Metadata } from 'next'
  
 export const metadata: Metadata = {
